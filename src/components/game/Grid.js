9db700490643cd072c7dc6852c64cbc41c1f962a
Micah <micahjank@gmail.com>
2020-06-17 17:16:57 -0500

    working on getting cords done in the board

--- src/components/game/Grid.js
+++ src/components/game/Grid.js
@@ -6,13 +6,20 @@ import '../../scss/Grid.scss';
 const Grid = () => {
     // height of the board can be determined by the size of each cell since i know that we need a 25 by 25 cell grid
     const cellSize = 20;
-    const boardSize = cellSize * 25;
+
+    const boardHeight = 500;
+    const boardWidth = 500;
+
+    // caclulate how many rows and columns there are
+    const boardRows = boardHeight / cellSize;
+    const boardCols = boardWidth / cellSize;
     // Tracks board state
-    const [board, setBoard] = useState([]);
+    const [board, setBoard] = useState({ columns: Array.from({ length: boardCols }, (x, i) => i), rows: Array.from({ length: boardRows }, (y, i) => i), cord: []});
     const [cells, setCells] = useState([])
 
+    console.log(board)
     return (
-        <div style={ { height: `${boardSize}px`, width: `${boardSize}px`, backgroundSize: `${cellSize}px ${cellSize}px` } } className="grid">
+        <div style={ { height: `${boardHeight}px`, width: `${boardWidth}px`, backgroundSize: `${cellSize}px ${cellSize}px` } } className="grid">
            
         </div>
     )
