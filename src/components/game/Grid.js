cdb04fe7a8a32a3adab2615a11a02c77b5b38920
Micah <micahjank@gmail.com>
2020-06-17 15:01:51 -0500

    created the grid and started work on making the cells

--- src/components/game/Grid.js
+++ src/components/game/Grid.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import '../../scss/Grid.scss';
 
 
 const Grid = () => {
+    // height of the board can be determined by the size of each cell since i know that we need a 25 by 25 cell grid
+    const cellSize = 20;
+    const boardSize = cellSize * 25;
+    // Tracks board state
+    const [board, setBoard] = useState([]);
+    const [cells, setCells] = useState([])
 
     return (
-        <div className="grid">
-            Grid
+        <div style={ { height: `${boardSize}px`, width: `${boardSize}px`, backgroundSize: `${cellSize}px ${cellSize}px` } } className="grid">
+           
         </div>
     )
 }
