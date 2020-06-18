63a58e5f4cfa203daa61106dffff9a08276540f0
MicahJank <MicahJank@users.noreply.github.com>
2020-06-17 21:45:20 -0500

    made the cells into buttons

--- src/components/game/Cell.js
+++ src/components/game/Cell.js
@@ -4,9 +4,9 @@ import React, { useState, useEffect } from 'react';
 const Cell = () => {
 
     return (
-        <div style={{ width: '20px', height: '20px'}} className="cell-block">
+        <button style={{ width: '20px', height: '20px'}} className="cell-block">
 
-        </div>
+        </button>
     )
 }
 
