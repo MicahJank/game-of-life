2ff33ae2c69c3dd9a60b52845cf95cdb9e72bc98
MicahJank <MicahJank@users.noreply.github.com>
2020-06-17 21:42:22 -0500

    cells are in place now

---
+++ src/components/game/Cell.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+
+const Cell = () => {
+
+    return (
+        <div style={{ width: '20px', height: '20px'}} className="cell-block">
+
+        </div>
+    )
+}
+
+export default Cell;
--- src/components/game/Grid.js
+++ src/components/game/Grid.js
@@ -1,26 +1,63 @@
 import React, { useState, useEffect } from 'react';
 
+import Cell from './Cell.js';
+
 import '../../scss/Grid.scss';
 
+ // height of the board can be determined by the size of each cell since i know that we need a 25 by 25 cell grid
+ const cellSize = 20;
 
-const Grid = () => {
-    // height of the board can be determined by the size of each cell since i know that we need a 25 by 25 cell grid
-    const cellSize = 20;
+ const boardHeight = 500;
+ const boardWidth = 500;
 
-    const boardHeight = 500;
-    const boardWidth = 500;
+// caclulate how many rows and columns there are
+const boardRows = boardHeight / cellSize;
+const boardCols = boardWidth / cellSize;
 
-    // caclulate how many rows and columns there are
-    const boardRows = boardHeight / cellSize;
-    const boardCols = boardWidth / cellSize;
+const createBoard = (numOfCols, numOfRows) => {
+//    const board = { 
+//         columns: Array.from({ length: numOfCols }, (x, i) => i), 
+//         rows: Array.from({ length: numOfRows }, (y, i) => i), 
+//         cord: []
+//     }
+
+    let board = new Array(numOfRows);
+
+    for (let y = 0; y < numOfRows; y++) {
+        board[y] = new Array(numOfCols);
+    }
+
+    return board;
+}
+
+const createCells = () => {
+    let cellArray = [];
+
+    for (let i = 0; i < boardRows; i++) {
+
+        for (let j = 0; j < boardCols; j++) {
+            cellArray.push(<Cell />)
+        }
+    }
+
+    return cellArray;
+}
+
+const Grid = () => {  
     // Tracks board state
-    const [board, setBoard] = useState({ columns: Array.from({ length: boardCols }, (x, i) => i), rows: Array.from({ length: boardRows }, (y, i) => i), cord: []});
-    const [cells, setCells] = useState([])
+    const [board, setBoard] = useState([]);
+    const [cells, setCells] = useState([]);
 
-    console.log(board)
+    useEffect(() => {
+        setBoard(createBoard(boardCols, boardRows));
+        setCells(createCells());
+    }, [])
+
+    console.log(cells);
+
     return (
         <div style={ { height: `${boardHeight}px`, width: `${boardWidth}px`, backgroundSize: `${cellSize}px ${cellSize}px` } } className="grid">
-           
+            {cells}
         </div>
     )
 }
